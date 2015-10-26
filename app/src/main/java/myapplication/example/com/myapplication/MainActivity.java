928b154f845e3795c1b2f3f63a4f720fa6ac363d
rafran07 <rafran07@louisville.edu>
2015-10-26 16:08:28 -0400

    Initial commit

---
+++ app/src/main/java/myapplication/example/com/myapplication/MainActivity.java
@@ -0,0 +1,34 @@
+package myapplication.example.com.myapplication;
+
+        import android.os.Bundle;
+        import android.app.Activity;
+        import android.view.Menu;
+        import android.content.Intent;
+        import android.view.View;
+
+        import myapplication.example.com.myapplication.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    // Method to start the service
+    public void startService(View view) {
+        startService(new Intent(getBaseContext(), MyService.class));
+    }
+
+    // Method to stop the service
+    public void stopService(View view) {
+        stopService(new Intent(getBaseContext(), MyService.class));
+    }
+}
